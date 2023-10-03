f35d37fb9ceaf9710f2b9521d2a65358b9fa66a3
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2023-10-03 15:51:24 +0900

    Update Bard_Chatbot.py

--- Bard_Chatbot.py
+++ Bard_Chatbot.py
@@ -24,16 +24,16 @@ API_KEY = st.sidebar.text_input(":blue[Enter Your OPENAI API-KEY :key:]",
 os.environ["_BARD_API_KEY"] = API_KEY
 
 
-session = requests.Session()
-session.headers = {
-            "Host": "bard.google.com",
-            "X-Same-Domain": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "Origin": "https://bard.google.com",
-            "Referer": "https://bard.google.com/",
-        }
-session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY")) 
+# session = requests.Session()
+# session.headers = {
+#             "Host": "bard.google.com",
+#             "X-Same-Domain": "1",
+#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+#             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+#             "Origin": "https://bard.google.com",
+#             "Referer": "https://bard.google.com/",
+#         }
+# session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY")) 
 
 
 
